fd6823fdd6d4f519d4b6a09a819277a031536cac
Shrawan-git <shrawanshaky3@gmail.com>
2020-09-02 13:09:56 +0545

    Pause Menu created

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
    public GameObject Pausemenu, PauseButton;
 
    public void Pause(){
+       Pausemenu.SetActive (true); //pause menu shown
+       PauseButton.SetActive(false); //pause button unclickable
        Time.timeScale = 0;
+   }
+   
+   public void Resume(){
+       Pausemenu.SetActive (false); //opposite to pause
+       PauseButton.SetActive(true);
+       Time.timeScale = 1;
+   }
+
+   public void LoadMenu(){
+       Time.timeScale = 1f;
+    SceneManager.LoadScene("MainMenu");
+   }
+
+   public void QuitGame(){
+    Debug.Log("quit menu");
+    Application.Quit();
    }
    }
